7827e6974b576af828a02520a30afb546b5be56b
Testing_author <testing_beta@mail.com>
2020-03-14 11:29:07 +0200

    Added the property which will contain the image selected by the User.
    Allowing the user to import photos from the user library.
    
    Making viewController conform to delegate UIImagePickerControllerDelegate and UINavigationControllerDelegate.
    
    Modifying the .plist to allow editing the photo.

--- InstaFrame_app/ViewController.swift
+++ InstaFrame_app/ViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "InstaFrame"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
     @IBAction func changeFilter(_ sender: Any) {
     }
     @IBAction func save(_ sender: Any) {
